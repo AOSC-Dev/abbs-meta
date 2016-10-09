595d6d0505efbf00ce4696bde8eb401474296663
liushuyu <liushuyu011@gmail.com>
2016-10-08 23:53:24 -0600

    Slightly speed up and format
    Using StringIO instead of tempfile, using autopep8 to format

--- abbsmeta.py
+++ abbsmeta.py
@@ -3,55 +3,58 @@
 
 import os
 import re
-import sys
 import sqlite3
 import logging
-import tempfile
 import subprocess
 import collections
+import io
 
-logging.basicConfig(format='%(asctime)s %(levelname).1s %(message)s', level=logging.INFO)
+logging.basicConfig(
+    format='%(asctime)s %(levelname).1s %(message)s', level=logging.INFO)
 
 re_variable = re.compile(r'^\s*([a-zA-Z_][a-zA-Z0-9_]*)=')
 re_packagename = re.compile(r'^([a-z0-9][a-z0-9+.-]*)(.*)$')
 
+
 def init_db(cur):
     cur.execute('CREATE TABLE IF NOT EXISTS packages ('
-        'name TEXT PRIMARY KEY,' # coreutils
-        'category TEXT,' # base
-        'section TEXT,' # utils
-        'pkg_section TEXT,' # (PKGSEC)
-        'version TEXT,' # 8.25
-        'release TEXT,' # None
-        'description TEXT'
-    ')')
+                'name TEXT PRIMARY KEY,'  # coreutils
+                'category TEXT,'  # base
+                'section TEXT,'  # utils
+                'pkg_section TEXT,'  # (PKGSEC)
+                'version TEXT,'  # 8.25
+                'release TEXT,'  # None
+                'description TEXT'
+                ')')
     cur.execute('CREATE TABLE IF NOT EXISTS package_spec ('
-        'package TEXT,'
-        'key TEXT,'
-        'value TEXT,'
-        'PRIMARY KEY (package, key)'
-    ')')
+                'package TEXT,'
+                'key TEXT,'
+                'value TEXT,'
+                'PRIMARY KEY (package, key)'
+                ')')
     cur.execute('CREATE TABLE IF NOT EXISTS package_dependencies ('
-        'package TEXT,'
-        'dependency TEXT,'
-        'version TEXT,'
-        # PKGDEP, PKGRECOM, PKGBREAK, PKGCONFL, PKGREP, BUILDDEP
-        'relationship TEXT,'
-        'PRIMARY KEY (package, dependency, relationship),'
-        'FOREIGN KEY(package) REFERENCES packages(name)'
-        # we may have unmatched dependency package name
-        # 'FOREIGN KEY(dependency) REFERENCES packages(name)'
-    ')')
+                'package TEXT,'
+                'dependency TEXT,'
+                'version TEXT,'
+                # PKGDEP, PKGRECOM, PKGBREAK, PKGCONFL, PKGREP, BUILDDEP
+                'relationship TEXT,'
+                'PRIMARY KEY (package, dependency, relationship),'
+                'FOREIGN KEY(package) REFERENCES packages(name)'
+                # we may have unmatched dependency package name
+                # 'FOREIGN KEY(dependency) REFERENCES packages(name)'
+                ')')
     cur.execute('CREATE INDEX IF NOT EXISTS idx_package_dependencies'
                 ' ON package_dependencies (package)')
 
-def uniq(seq): # Dave Kirby
+
+def uniq(seq):  # Dave Kirby
     # Order preserving
     seen = set()
     return [x for x in seq if x not in seen and not seen.add(x)]
 
+
 def read_bash_vars(filename):
-    with open(filename, 'r') as sh, tempfile.NamedTemporaryFile('w') as tmpf:
+    with open(filename, 'r') as sh, io.StringIO() as tmpf:
         var = []
         for ln in sh:
             match = re_variable.match(ln)
@@ -63,13 +66,14 @@ def read_bash_vars(filename):
         for v in var:
             tmpf.write('echo "$%s"\n' % v)
         tmpf.flush()
-        outs, errs = subprocess.Popen(('bash', tmpf.name),
-            stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+        outs, errs = subprocess.Popen(tmpf.getvalue(),
+                                      stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).communicate()
         if errs:
             logging.warning('%s: %s', filename, errs.decode().rstrip())
         lines = outs.decode().splitlines()
         assert len(var) == len(lines)
         return collections.OrderedDict(zip(var, lines))
+
 
 def scan_abbs_tree(cur, basepath):
     categories = ('base-', 'extra-')
@@ -89,11 +93,12 @@ def scan_abbs_tree(cur, basepath):
                     if filename != 'defines':
                         continue
                     pkgspec = spec.copy()
-                    pkgspec.update(read_bash_vars(os.path.join(dirpath, 'defines')))
+                    pkgspec.update(read_bash_vars(
+                        os.path.join(dirpath, 'defines')))
                     name = pkgspec.pop('PKGNAME', None)
                     if not name:
                         # we assume it is a define for some specific architecture
-                        #print(dirpath, pkgspec)
+                        # print(dirpath, pkgspec)
                         continue
                     section2 = pkgspec.pop('PKGSEC', None)
                     description = pkgspec.pop('PKGDES', None)
@@ -102,13 +107,17 @@ def scan_abbs_tree(cur, basepath):
                     dependencies = []
                     for rel in ('PKGDEP', 'PKGRECOM', 'PKGBREAK', 'PKGCONFL', 'PKGREP', 'BUILDDEP'):
                         for pkgname in pkgspec.pop(rel, '').split():
-                            deppkg, depver = re_packagename.match(pkgname).groups()
+                            deppkg, depver = re_packagename.match(
+                                pkgname).groups()
                             dependencies.append((name, deppkg, depver, rel))
                     cur.execute('REPLACE INTO packages VALUES (?,?,?,?,?,?,?)',
-                        (name, category, section, section2, version, release, description))
+                                (name, category, section, section2, version, release, description))
                     for k, v in pkgspec.items():
-                        cur.execute('REPLACE INTO package_spec VALUES (?,?,?)', (name, k, v))
-                    cur.executemany('REPLACE INTO package_dependencies VALUES (?,?,?,?)', dependencies)
+                        cur.execute(
+                            'REPLACE INTO package_spec VALUES (?,?,?)', (name, k, v))
+                    cur.executemany(
+                        'REPLACE INTO package_dependencies VALUES (?,?,?,?)', dependencies)
+
 
 def main(dbfile, path):
     db = sqlite3.connect(dbfile)
