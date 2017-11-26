728bb9606389f9dfc492c49183b37fd9b6d26ad4
Dingyuan Wang <abcdoyle888@gmail.com>
2017-11-26 14:51:41 +0800

    output patch

--- tools/addchksum.sh
+++ tools/addchksum.sh
@@ -5,6 +5,6 @@ CURDIR="$PWD"
 for tree in aosc-os-core aosc-os-abbs aosc-os-arm-bsps; do
     pushd "$tree"
     find . -maxdepth 3 -type f -name spec | \
-        parallel -j4 --bar "python3 $DIR/addchksum.py {} 2>$CURDIR/chksumerr.log"
+        parallel -j4 --bar "python3 $DIR/addchksum.py {} 2>$CURDIR/chksumerr.log" > "$CURDIR"/"$tree".patch
     popd
 done
